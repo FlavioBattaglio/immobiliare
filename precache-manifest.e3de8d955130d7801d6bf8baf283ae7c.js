98d2c73852bed894a787c7fdd796489e8663963d
FlavioBattaglio <flaviobattaglio@hotmail.it>
2025-07-06 04:26:03 +0200

    Updates

---
+++ precache-manifest.e3de8d955130d7801d6bf8baf283ae7c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb952e03bbde90ee4498ee4a5c4d5313",
+    "url": "/immobiliare/index.html"
+  },
+  {
+    "revision": "7090fc9105e772e69109",
+    "url": "/immobiliare/static/css/2.b8b4089c.chunk.css"
+  },
+  {
+    "revision": "c14bec7e1943b0fd5b61",
+    "url": "/immobiliare/static/css/main.7eaea7c1.chunk.css"
+  },
+  {
+    "revision": "7090fc9105e772e69109",
+    "url": "/immobiliare/static/js/2.008ba21b.chunk.js"
+  },
+  {
+    "revision": "747ef093c0efba43f1ef",
+    "url": "/immobiliare/static/js/3.cdf3fc0b.chunk.js"
+  },
+  {
+    "revision": "c14bec7e1943b0fd5b61",
+    "url": "/immobiliare/static/js/main.438c04aa.chunk.js"
+  },
+  {
+    "revision": "9281ee32c0ecf22fdea4",
+    "url": "/immobiliare/static/js/runtime~main.23723035.js"
+  },
+  {
+    "revision": "201a17e5c99762fd34a6affb6d83050b",
+    "url": "/immobiliare/static/media/backvideo.201a17e5.mp4"
+  },
+  {
+    "revision": "84a4d15b9e4459b6229a7f1dccc6609c",
+    "url": "/immobiliare/static/media/bootstrap-icons.84a4d15b.woff2"
+  },
+  {
+    "revision": "efe54401ef52214fd3b701e23ff88c5f",
+    "url": "/immobiliare/static/media/bootstrap-icons.efe54401.woff"
+  },
+  {
+    "revision": "8509fc1c7af0699a56eb792abafc4c2c",
+    "url": "/immobiliare/static/media/female1.8509fc1c.png"
+  },
+  {
+    "revision": "43cb4c28053db4a60c59af7396b23d91",
+    "url": "/immobiliare/static/media/male1.43cb4c28.png"
+  },
+  {
+    "revision": "b45d946484de0a6ea601e01b324d42e5",
+    "url": "/immobiliare/static/media/male2.b45d9464.png"
+  },
+  {
+    "revision": "5b3ea290a6ebe7eebfd8b4e491075320",
+    "url": "/immobiliare/static/media/male3.5b3ea290.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/immobiliare/precache-manifest.e3de8d955130d7801d6bf8baf283ae7c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/immobiliare/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
